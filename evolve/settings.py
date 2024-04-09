d94cc41d30779fd2ad682e3a31254615468bc6ea
Ruslan Shafikov <rs31113@yandex.ru>
2024-04-09 18:07:22 +0300

    fix static issue

--- evolve/settings.py
+++ evolve/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -104,10 +105,11 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 

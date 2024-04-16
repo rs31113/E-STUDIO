05eda2cfd32175d2e57324634e2abc81aad59387
Ruslan Shafikov <rs31113@yandex.ru>
2024-04-16 21:57:58 +0300

    fix proportions

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,7 @@ window.addEventListener("touchmove", handleTouchMove, { passive: true });
 function handleScroll(event) {
     if (event.deltaY > 0 || event.touches) {
         document.getElementById("e-studio").style.fontSize = "30px";
+        document.getElementById("e-studio").style.marginTop = "20px";
         document.querySelector(".container").style.opacity = "1";
         document.querySelector(".footer").style.opacity = "1";
         document.getElementById("arrow-down").style.opacity = "0";
@@ -16,6 +17,7 @@ function handleScroll(event) {
         }, 500);
     } else if (window.scrollY === 0 && (event.deltaY < 0 || event.touches)) {
         document.getElementById("e-studio").style.fontSize = "10vw";
+        document.getElementById("e-studio").style.marginTop = "100px";
         document.querySelector(".container").style.opacity = "0";
         document.querySelector(".footer").style.opacity = "0";
         document.getElementById("arrow-down").style.opacity = "1";

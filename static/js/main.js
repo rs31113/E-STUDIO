704bf8c2d492282c7e78b2f9fd30d1e74d684085
Ruslan Shafikov <rs31113@yandex.ru>
2024-04-16 18:51:55 +0300

    fix proportions; add swipe support

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
-window.addEventListener("wheel", function(event) {
-    if (event.deltaY > 0) {
+window.addEventListener("wheel", handleScroll, { passive: true });
+window.addEventListener("touchstart", handleTouchStart, { passive: true });
+window.addEventListener("touchmove", handleTouchMove, { passive: true });
+
+function handleScroll(event) {
+    if (event.deltaY > 0 || event.touches) {
         document.getElementById("e-studio").style.fontSize = "30px";
         document.querySelector(".container").style.opacity = "1";
         document.querySelector(".footer").style.opacity = "1";
@@ -10,7 +14,7 @@ window.addEventListener("wheel", function(event) {
         setTimeout(function() {
             document.querySelector(".worldwide").style.display = "none";
         }, 500);
-    } else if (window.scrollY === 0 && event.deltaY < 0) {
+    } else if (window.scrollY === 0 && (event.deltaY < 0 || event.touches)) {
         document.getElementById("e-studio").style.fontSize = "10vw";
         document.querySelector(".container").style.opacity = "0";
         document.querySelector(".footer").style.opacity = "0";
@@ -21,4 +25,16 @@ window.addEventListener("wheel", function(event) {
             document.querySelector(".worldwide").style.opacity = "1";
         }, 10);
     }
-}, { passive: true });
+}
+
+let touchStartY;
+
+function handleTouchStart(event) {
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    if (event.touches[0].clientY > touchStartY || window.scrollY === 0) {
+        handleScroll(event);
+    }
+}
